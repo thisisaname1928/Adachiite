aa2cf82445e13337b236d6bc515de2284a0b9d5f
ngqt <nguyenquoctrungap47@gmail.com>
2025-07-22 15:08:31 +0700

    rewrite; use header from gnu-efi for less pain

--- main.c
+++ main.c
@@ -1,71 +1,12 @@
-#include "adachiite/adachiite.h"
-#include "efi/efi.h"
-#include <stdint.h>
+#include "inc/efi.h"
+#include "inc/efiapi.h"
+#include "inc/efierr.h"
 
-void hlt() {
+EFI_STATUS efi_main(EFI_HANDLE imageHandle, EFI_SYSTEM_TABLE *sysTab) {
+
+  sysTab->ConOut->OutputString(sysTab->ConOut, L"HIII");
+
   for (;;) {
-    __asm__ volatile("hlt");
   }
-}
-
-EFI_SYSTEM_TABLE *sysTab;
-EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *outConsole;
-EFI_SIMPLE_TEXT_INPUT_PROTOCOL *inConsole;
-EFI_BOOT_SERVICES *bootServices;
-
-EFI_STATUS EFIAPI efi_main(EFI_HANDLE imageHandle,
-                           EFI_SYSTEM_TABLE *systemTable) {
-  sysTab = systemTable;
-  outConsole = sysTab->consoleOut;
-  inConsole = sysTab->consoleIn;
-  bootServices = sysTab->bootServices;
-
-  enableCursor();
-  clearScreen();
-
-  EFI_GUID sfs = EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID;
-  uint64_t n = 0;
-  EFI_HANDLE *h;
-  EFI_STATUS s = bootServices->locateHandleBuffer(byProtocol, &sfs, 0, &n, &h);
-
-  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *interface;
-  s = bootServices->openProtocol(h[0], &sfs, (void *)&interface, imageHandle, 0,
-                                 EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
-
-  print(u"\n");
-
-  if (s != 0) {
-    print(u"UUUUUU");
-  }
-
-  EFI_FILE_PROTOCOL *f;
-  s = interface->openVolume(interface, &f);
-  uintn bf = 90;
-  EFI_FILE_INFO *buffer = allocPool(90);
-  uint64_t currentPos = 0;
-
-  f->setPos(f, 2);
-  s = f->read(f, &bf, buffer);
-  printUint(bf);
-  if (s != 0)
-    print(u"HAI");
-  // s = f->read(f, &bf, buffer);
-
-  print(buffer->FileName);
-
-  print(u"\n\r");
-
-  print(u"\r\nadachiite> ");
-  resetInput();
-
-  while (true) {
-    uint16_t a[2];
-    a[1] = 0;
-
-    a[0] = readKey();
-    print(a);
-  }
-
-  hlt();
-  return 0;
+  return EFI_SUCCESS;
 }
